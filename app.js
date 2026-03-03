e5746bbadee9647be09ca8003df3d44fae5d5e77
Geral <144078119+GeralDuran@users.noreply.github.com>
2026-03-02 21:53:19 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+// Hacemos las comparaciones logicas
+
+// Hacemos or 
+
+console.log("Lógica or");
+
+let a = false;
+let b = false;
+console.log(a || b); 
+
+a = false;
+b = true;
+console.log(a || b); 
+
+a = true;
+b = false;
+console.log(a || b); 
+
+a = true;
+b = true;
+console.log(a || b); 
+
+//Hacemos lo mismo con and
+
+console.log("Lógica and");
+
+let a1 = false;
+let b1 = false;
+console.log(a1 && b1); 
+
+a1 = false;
+b1 = true;
+console.log(a1 && b1); 
+
+a1 = true;
+b1 = false;
+console.log(a1 && b1); 
+
+a1 = true;
+b1 = true;
+console.log(a1 && b1); 
+
